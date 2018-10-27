02812a5660487f9bead3fbcc2d959069b4671b81
valerik chobanyan <valerik.chobanyan@gmail.com>
2018-10-27 09:13:22 +0400

    kk

--- 20.py
+++
@@ -1,7 +0,0 @@
-from sources import daily, weekly
-print("Ежедневный прогноз:", daily.forecast())
-print("Еженедельный прогноз:")
-for number, outlook in enumerate(weekly.forecast(), 1):
-    print(number, outlook)
-
-
